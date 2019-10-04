bbd1a0a320850f81537907f3cc1c5ae3f2fd654c
Ahmed Saleh Sobeih <aproram@gmail.com>
2019-10-04 06:09:32 +0200

    * .gitignore: initial commit
    * httpclientbug.sln:
    * Main.cs:
    * Info.plist:
    * AppDelegate.cs:
    * Main.storyboard:
    * SceneDelegate.cs:
    * ViewController.cs:
    * Entitlements.plist:
    * httpclientbug.csproj:
    * LaunchScreen.storyboard:
    * LaunchScreen.xib:
    * AssemblyInfo.cs:
    * ViewController.designer.cs:
    * Icon58.png:
    * Icon80.png:
    * Icon87.png:
    * Icon40.png:
    * Icon29.png:
    * Icon20.png:
    * Icon60.png:
    * Icon76.png:
    * Icon180.png:
    * Icon167.png:
    * Icon152.png:
    * Icon120.png:
    * Icon1024.png:
    * Contents.json:

---
+++ httpclientbug/ViewController.cs
@@ -0,0 +1,50 @@
+﻿using Foundation;
+using System;
+using System.IO;
+using System.Net.Http;
+using UIKit;
+
+namespace httpclientbug
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            FetchtheJSON();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        public async void FetchtheJSON()
+        {
+            HttpClient client = new HttpClient();
+
+            var Doctor_JSON_From_API = new Uri("https://www.stanfordchildrens.org/api/m/prod-en-doctors.json");
+            txtbox1.Text = "Service fetching started..";
+            var response = await client.GetAsync(Doctor_JSON_From_API);
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStreamAsync();
+                //Console.WriteLine("JSON Is Fetched");
+                using (var streamReader = new StreamReader(content))
+                {
+                    var content_json = streamReader.ReadToEnd();
+                    //Console.WriteLine("Query Result: " + content_json);
+                    txtbox1.Text = content_json;
+                }
+
+                //Console.WriteLine(content_string);
+            }
+        }
+    }
+}
